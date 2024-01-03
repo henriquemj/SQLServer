390c5e727d68e4deadd691e886599ff204565938
HenriqueDev <henriquemadureira05@gmail.com>
2024-01-02 21:45:08 -0300

    Add tb_Produtos

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -35,10 +35,26 @@ Loja de Sucos do Henrique
 
 	"Coxinha" (7 Letras) -> Cox
 
-*/
+teste */
 
 
-CREATE TABLE tb_DadosProduto (
+-- Aula 4 - Criando Base de dados (Parte 1)
+
+CREATE TABLE tb_Produtos (
+	cdProduto INT PRIMARY KEY IDENTITY(1,1),
 	nmProduto VARCHAR(50) NOT NULL, -- Obrigatório
 	vlProduto DECIMAL(6,2) NOT NULL
 );
+
+-- Aula 5 - Criando Base de dados (Parte 2)
+
+CREATE TABLE tb_Clientes (
+	nmCliente VARCHAR(50) NOT NULL,
+	dtNascimento DATE,
+	inSexo VARCHAR(1) NOT NULL,
+	nmEndereço VARCHAR(50),
+	nmCidade VARCHAR(50),
+	nmEstado VARCHAR(30),
+	nmTelefone1 VARCHAR(15),
+	nmTelefone2 VARCHAR(15)
+)
