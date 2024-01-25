2f075c02082e362dca19f877a5f30ab979016e46
HenriqueDev <henriquemadureira05@gmail.com>
2024-01-24 21:16:40 -0300

    query inner join

--- Aulas - InnerJoin.sql
+++ Aulas - InnerJoin.sql
@@ -5,6 +5,7 @@ SELECT * FROM tb_ProdutoVenda
 SELECT * FROM tb_Produtos
 
 -- Seleção dos dados com valores
-SELECT P.cdProduto, P.nmProduto, PV.qtProduto, P.vlProduto
-FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV
-ON P.cdProduto = PV.cdProduto
+SELECT P.cdProduto, C.nmCliente, P.nmProduto, PV.qtProduto, P.vlProduto
+FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.cdProduto = PV.cdProduto
+INNER JOIN tb_vendas V ON PV.cdVenda = V.cdVenda
+INNER JOIN tb_Clientes C ON V.cdCliente = C.cdCliente
